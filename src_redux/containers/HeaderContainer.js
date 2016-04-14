b9fab319f25a5abd165d588563260843a51ae910
Panepo <whisper.ko@gmail.com>
2016-04-14 14:22:15 +0800

    redux: add an action

--- src_redux/containers/HeaderContainer.js
+++ src_redux/containers/HeaderContainer.js
@@ -1,24 +1,17 @@
 import React, { Component, PropTypes } from 'react'
 import { connect } from 'react-redux'
 import { CbuttonActive } from '../constants/ClassName'
-import { modelOpen, modelClose } from '../actions'
-import ImgModel from '../components/ImgModel'
+import { modelOpen } from '../actions'
+import ImgModelContainer from './ImgModelContainer'
 import '../css/Header.css'
 
 class HeaderContainer extends Component {
 	render() {
-		const { img0State, img1State, modelOpen, modelClose } = this.props
+		const { modelOpen } = this.props
 		
 		return (
 			<header className="demo-header mdl-layout__header mdl-layout__header--scroll mdl-color--grey-100 mdl-color-text--grey-800">
-				<ImgModel
-					display={img0State}
-					imgSrc={"./img/char.jpg"} 
-					modelClose={() => modelClose('modal0')} />
-				<ImgModel
-					display={img1State}
-					imgSrc={"./img/gift.jpg"}
-					modelClose={() => modelClose('modal1')} />
+				<ImgModelContainer />
 				<div className="mdl-layout__header-row">
 					<span className="mdl-layout-title"></span>
 					<div className="mdl-layout-spacer"></div>
@@ -38,20 +31,16 @@ class HeaderContainer extends Component {
 }
 
 HeaderContainer.propTypes = {
-	img0State: PropTypes.bool.isRequired,
-	img1State: PropTypes.bool.isRequired,
-	modelOpen: PropTypes.func.isRequired,
-	modelClose: PropTypes.func.isRequired
+	modelOpen: PropTypes.func.isRequired
 }
 
 const mapStateToProps = (state) => {
 	return {
-		img0State: state.imageModel.modal0Display,
-		img1State: state.imageModel.modal1Display
+		
 	}
 }
 
 export default connect(
 	mapStateToProps,
-	{ modelOpen, modelClose }
+	{ modelOpen }
 )(HeaderContainer)
